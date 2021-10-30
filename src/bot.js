1092eb25bef5cc8a874ec8f6014461d755d4640c
Brando112 <77802283+Brando112@users.noreply.github.com>
2021-10-30 11:03:46 -0500

    Added fishing with no database

--- src/bot.js
+++ src/bot.js
@@ -14,6 +14,62 @@ var conn = mysql.createConnection({
     database: "discordbot"
 })
 
+function fish(){
+    var randnum = Math.floor(Math.random() * (10000 - 1 + 1) ) + 1;
+    let base_chance = 1100
+    let rare_chance = 16
+    let legendary_chance = 1
+
+    if (randnum < (base_chance)){ // Alaskan cod
+        return 'Alaskan Cod'
+    }
+    else if (randnum < (base_chance*2)){ // Bass
+        return 'Bass'
+    }
+    else if (randnum < (base_chance*3)){ // Carp
+        return 'Carp'
+    }
+    else if (randnum < (base_chance*4)){ // Catfish
+        return 'Catfish'
+    }
+    else if (randnum < (base_chance*5)){ // Peruvian Anchoveta
+        return 'Peruvian Anchoveta'
+    }
+    else if (randnum < (base_chance*6)){ // Tuna
+        return 'Tuna'
+    }
+    else if (randnum < (base_chance*7)){ // Salmon
+        return 'Salmon'
+    }
+    else if (randnum < (base_chance*8)){ // Tilapia
+        return 'Tilapia'
+    }
+    else if (randnum < (base_chance*9)){ // Alaskan Pollock
+        return 'Alaskan Pollock'
+    }
+    else if (randnum < (base_chance*9) + rare_chance){ // Coelacanth
+        return 'Coelacanth'
+    }
+    else if (randnum < (base_chance*9) + rare_chance*2){ // Sturgeon
+        return 'Sturgeon'
+    }
+    else if (randnum < (base_chance*9) + rare_chance*3){ // Eel
+        return 'Eel'
+    }
+    else if (randnum < (base_chance*9) + rare_chance*4){ // Smalltooth Sawfish
+        return 'Smalltooth Sawfish'
+    }
+    else if (randnum < (base_chance*9) + rare_chance*5){ // Big Catfish
+        return 'Big Catfish'
+    }
+    else if (randnum < (base_chance*9) + rare_chance*6){ // Sunfish
+        return 'Sunfish'
+    }
+    else if (randnum < (base_chance*9) + (rare_chance*6) + (legendary_chance +3)){ // Devils Hole Pupfish
+        return 'Devils Hole Pupfish'
+    }
+}
+
 client.on('ready', () => {
     console.log("Bot is online")
 
@@ -31,6 +87,11 @@ client.on('ready', () => {
     commands?.create({
         name: 'ping',
         description: 'Replies with pong.'
+    })
+
+    commands?.create({
+        name: 'fish',
+        description: "Fishes up a random fish."
     })
 
     commands?.create({
@@ -80,6 +141,13 @@ client.on('interactionCreate', async (Interaction) => {
         Interaction.reply({
             content: `The sum is ${num1 + num2}`, // Keep in mind that template literals uses "grave" instead of single quotes
             ephemeral: true
+        })
+    }
+    else if (commandName === 'fish'){
+        var fish_caught = fish()
+        Interaction.reply({
+            content: `Congrats you caught a(n) ${fish_caught}`,
+            ephemeral: false
         })
     }
 })
