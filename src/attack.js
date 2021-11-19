8643f15fcfd7faacf1cc49ddcec1a839cf64f647
Brando112 <77802283+Brando112@users.noreply.github.com>
2021-11-18 21:19:24 -0600

    better spawn funciton

--- src/attack.js
+++ src/attack.js
@@ -25,5 +25,34 @@ module.exports =
             default:
                 break;
         }
+    },
+
+    spawnEnemy: function()
+    {
+        const enemies = ["Skeleton", "Zombie", "Goblin", "Mercenary"]
+        enemy_chosen = Math.floor(Math.random() * ((enemies.length) - 0 + 1)+1);
+        enemy_spawned = enemies[enemy_chosen]
+        let base_hp = 50
+        switch (enemy_spawned)
+        {
+            case "Skeleton":
+                let hp_mod = 50
+                let enemyHP =  Math.floor(Math.random() * ((base_hp+hp_mod) - base_hp + 1) + 1);
+                return ["Skeleton", enemyHP]
+            case "Zombie":
+                let hp_mod = 75
+                let enemyHP =  Math.floor(Math.random() * ((base_hp+hp_mod) - base_hp + 1) + 1);
+                return ["Zombie", enemyHP]
+            case "Goblin":
+                let hp_mod = 100
+                let enemyHP =  Math.floor(Math.random() * ((base_hp+hp_mod) - base_hp + 1) + 1);
+                return ["Goblin", enemyHP]
+            case "Mercenary":
+                let hp_mod = 150
+                let enemyHP =  Math.floor(Math.random() * ((base_hp+hp_mod) - base_hp + 1) + 1);
+                return ["Mercenary", enemyHP]    
+            default:
+                break;
+        }
     }
 }
